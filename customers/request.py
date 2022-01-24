09e30c4ec1ee54a5b0f67923f84d2d5fd642e4ab
Sullivan Pierce <sullypierce@gmail.com>
2022-01-24 09:27:08 -0500

    added get_customer_by_email function, is currently functional

--- customers/request.py
+++ customers/request.py
@@ -85,6 +85,33 @@ def get_single_customer(id):
                             data['email'], data['password'])
 
         return json.dumps(customer.__dict__)
+    
+def get_customers_by_email(email):
+
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        from Customer c
+        WHERE c.email = ?
+        """, ( email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'])
+            customers.append(customer.__dict__)
+
+    return json.dumps(customers)
 
 def create_customer(customer):
     # Get the id value of the last customer in the list
